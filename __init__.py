32642b9331674b551939286129c01afb7ebf33ab
Sergey Nartimov <just.lest@gmail.com>
2018-06-15 12:56:27 +0300

    Bump version

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ class BookFusionPlugin(InterfaceActionBase):
     description = 'BookFusion Plugin'
     supported_platforms = ['windows', 'osx', 'linux']
     author = 'BookFusion'
-    version = (0, 2, 0)
+    version = (0, 2, 1)
     minimum_calibre_version = (3, 16, 0)
 
     actual_plugin = 'calibre_plugins.bookfusion.ui:InterfacePlugin'
